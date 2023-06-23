d345fe5ff95ae281aa35b616c9608efd196c8eb1
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-06-23 17:46:57 -0300

    Implementation project

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -17,4 +17,19 @@ public class ParkingSpotService {
     public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
         return parkingSpotRepository.save(parkingSpotModel);
     }
+
+    @Transactional
+    public boolean existsByLicensePlateCar(String licensePlateCar){
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    @Transactional
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber){
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    @Transactional
+    public boolean existsByApartmentAndBlock(String apartment, String block){
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
 }
