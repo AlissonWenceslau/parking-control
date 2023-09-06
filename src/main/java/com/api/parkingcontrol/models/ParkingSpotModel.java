ef50139efda044343687b0a1c3990e35d4799e3a
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-09-06 14:44:23 -0300

    serialVersionUID

--- src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
+++ src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "TB_PARKING_SPOT")
 public class ParkingSpotModel implements Serializable {
-    private static final long serialVersionID = 1L;
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
