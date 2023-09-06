91a4a571ed969f9f0488b2a43e0666c51cf70fc1
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-09-06 15:08:28 -0300

    Transactional

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -5,23 +5,26 @@ import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class ParkingSpotService {
 
+	
     final ParkingSpotRepository parkingSpotRepository;
     public  ParkingSpotService(ParkingSpotRepository parkingSpotRepository){
         this.parkingSpotRepository = parkingSpotRepository;
     }
 
+    @Transactional(readOnly = true)
     public Page<ParkingSpotModel> findAll(Pageable pageable){
         return parkingSpotRepository.findAll(pageable);
     }
-
+    
+    @Transactional(readOnly = true)
     public Optional<ParkingSpotModel> fidById(UUID id){
         return parkingSpotRepository.findById(id);
     }
@@ -34,14 +37,18 @@ public class ParkingSpotService {
     public void deleteById(UUID id){
         parkingSpotRepository.deleteById(id);
     }
+    
+    @Transactional
     public boolean existsByLicensePlateCar(String licensePlateCar){
         return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
     }
 
+    @Transactional
     public boolean existsByParkingSpotNumber(String parkingSpotNumber){
         return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
     }
 
+    @Transactional
     public boolean existsByApartmentAndBlock(String apartment, String block){
         return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
     }
